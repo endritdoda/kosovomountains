9d1a334799b3c5ec1dee438ddbac752c3b0ec5db
endritdoda <diti_doda@hotmail.com>
2022-10-25 01:30:20 +0200

    Create DummyData.js

---
+++ src/utils/DummyData.js
@@ -0,0 +1,19 @@
+const menu = [
+    {
+        id:"#home",
+        path:"/",
+        name:"Home",
+
+    },  {
+        id:"#mali1",
+        path:"/mali1",
+        name:"Gjeravica",
+
+    },  {
+        id:"#mali2",
+        path:"/mali2",
+        name:"Maja e Zezë",
+
+    },
+]
+export {menu};
